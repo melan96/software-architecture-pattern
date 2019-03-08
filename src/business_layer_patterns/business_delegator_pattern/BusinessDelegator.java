1e238a993c85a0924872b0cfc161ca4d09f78a33
melan96 <it17006712@my.sliit.lk>
2019-03-08 20:32:13 +0530

    minor bug fixed(Delegator.java.class)

--- src/business_layer_patterns/business_delegator_pattern/BusinessDelegator.java
+++ src/business_layer_patterns/business_delegator_pattern/BusinessDelegator.java
@@ -2,22 +2,24 @@ package business_layer_patterns.business_delegator_pattern;
 
 public class BusinessDelegator {
 
-	
 	private BusinessService businessService;
 	private BusinessServiceLocator businessServiceLocator = new BusinessServiceLocator();
 	private String serviceName;
-	
+
 	public void setString(String serviceName) {
 		this.serviceName = serviceName;
 	}
-	
-	//executed process over user required modules
+
+	// executed process over user required modules
 	public void doProcess() {
-		businessService = businessServiceLocator.getService(serviceName);
-		
-		//execute process
-		businessService.executeService();
+		if (!this.serviceName.isEmpty()) {
+			businessService = businessServiceLocator.getService(serviceName);
+
+			// execute process
+			businessService.executeService();
+		} else {
+			System.out.println("Service Name should set before executing..");
+		}
 	}
-	
-	
+
 }
